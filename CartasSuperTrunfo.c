75488c1156275b8bda71aebbebbad07243e557d8
thiagoss1703 <thiagoss1703@gmail.com>
2025-03-20 01:47:16 +0000

    Criei as 8 cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,148 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    // Carta A01 do Super trunfo
+    char estado1[20] = "Bahia";
+    char carta1[20]= "A01";
+    char cidade1[20]= "Salvador";
+    float poculacao1= 2417678;
+    float area1= 693.8;
+    double pib1= 63;
+    int pontos1= 15;
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Carta 1: %s\n", carta1);
+    printf("Estado: %s\n", estado1);
+    printf("Nome da Cidade: %s\n", cidade1);
+    printf("População: %.f\n", poculacao1);
+    printf("Área: %.f Km²\n", area1);
+    printf("PIB de R$ %.f Bilhões\n", pib1);
+    printf("Números de Pontos Turisticos: %d\n",pontos1);
+
+// Carta A02 do Super trunfo
+   char estado2[20] = "Pernambuco";
+   char carta2[20]= "A02";
+   char cidade2[20]= "Recife";
+   float poculacao2= 1488920;
+   float area2= 218.8;
+   double pib2= 54;
+   int pontos2= 30;
+   
+   printf("\nCarta 2: %s\n", carta2);
+   printf("Estado: %s\n", estado2);
+   printf("Nome da Cidade: %s\n", cidade2);
+   printf("População: %.f\n", poculacao2);
+   printf("Área: %.f Km²\n", area2);
+   printf("PIB de R$ %.f Bilhões\n", pib2);
+   printf("Números de Pontos Turisticos: %d\n",pontos2);
+
+
+   // Carta A03 do Super trunfo
+   char estado3[20] = "Alagoas";
+   char carta3[20]= "A03";
+   char cidade3[20]= "Maceió";
+   float poculacao3= 957916;
+   float area3= 509.2;
+   double pib3= 27;
+   int pontos3= 5;
+
+   printf("\nCarta 3: %s\n", carta3);
+   printf("Estado: %s\n", estado3);
+   printf("Nome da Cidade: %s\n", cidade3);
+   printf("População: %.f\n", poculacao3);
+   printf("Área: %.f Km²\n", area3);
+   printf("PIB de R$ %.f Bilhões\n", pib3);
+   printf("Números de Pontos Turisticos: %d\n",pontos3);
+
+
+   // Carta A04 do Super trunfo
+   char estado4[20] = "Amapá";
+   char carta4[20]= "A04";
+   char cidade4[20]= "Macapá";
+   float poculacao4= 442933;
+   float area4= 6563.8;
+   double pib4= 23;
+   int pontos4= 8;
+
+   printf("\nCarta 4: %s\n", carta4);
+   printf("Estado: %s\n", estado4);
+   printf("Nome da Cidade: %s\n", cidade4);
+   printf("População: %.f\n", poculacao4);
+   printf("Área: %.f Km²\n", area4);
+   printf("PIB de R$ %.f Bilhões\n", pib4);
+   printf("Números de Pontos Turisticos: %d\n",pontos4);
+
+
+   // Carta A05 do Super trunfo
+   char estado5[20] = "Amazonas";
+   char carta5[20]= "A05";
+   char cidade5[20]= "Manaus";
+   float poculacao5= 2063689;
+   float area5= 11401;
+   double pib5= 103;
+   int pontos5= 8;
+
+   printf("\nCarta 5: %s\n", carta5);
+   printf("Estado: %s\n", estado5);
+   printf("Nome da Cidade: %s\n", cidade5);
+   printf("População: %.f\n", poculacao5);
+   printf("Área: %.f Km²\n", area5);
+   printf("PIB de R$ %.f Bilhões\n", pib5);
+   printf("Números de Pontos Turisticos: %d\n",pontos5);
+
+
+   // Carta A06 do Super trunfo
+   char estado6[20] = "Ceará";
+   char carta6[20]= "A06";
+   char cidade6[20]= "Fortalaza";
+   float poculacao6= 2428708;
+   float area6= 312.3;
+   double pib6= 73;
+   int pontos6= 8;
+
+   printf("\nCarta 6: %s\n", carta6);
+   printf("Estado: %s\n", estado6);
+   printf("Nome da Cidade: %s\n", cidade6);
+   printf("População: %.f\n", poculacao6);
+   printf("Área: %.f Km²\n", area6);
+   printf("PIB de R$ %.f Bilhões\n", pib6);
+   printf("Números de Pontos Turisticos: %d\n",pontos6);
+
+
+   // Carta A07 do Super trunfo
+   char estado7[20] = "Goiás";
+   char carta7[20]= "A07";
+   char cidade7[20]= "Goiânia";
+   float poculacao7= 1437366;
+   float area7= 729.2;
+   double pib7= 59;
+   int pontos7= 8;
+
+   printf("\nCarta 7: %s\n", carta7);
+   printf("Estado: %s\n", estado7);
+   printf("Nome da Cidade: %s\n", cidade7);
+   printf("População: %.f\n", poculacao7);
+   printf("Área: %.f Km²\n", area7);
+   printf("PIB de R$ %.f Bilhões\n", pib7);
+   printf("Números de Pontos Turisticos: %d\n",pontos7);
+
+
+   // Carta A08 do Super trunfo
+   char estado8[20] = "Pará";
+   char carta8[20]= "A08";
+   char cidade8[20]= "Belém";
+   float poculacao8= 1303403;
+   float area8= 1059.4;
+   double pib8= 33;
+   int pontos8= 10;
+
+   printf("\nCarta 8: %s\n", carta8);
+   printf("Estado: %s\n", estado8);
+   printf("Nome da Cidade: %s\n", cidade8);
+   printf("População: %.f\n", poculacao8);
+   printf("Área: %.f Km²\n", area8);
+   printf("PIB de R$ %.f Bilhões\n", pib8);
+   printf("Números de Pontos Turisticos: %d\n",pontos8);
+
 
     return 0;
 }
